e8c24680bb4cd5f8cdb427c93c1e9312bb816771
IPS Data Manager <101430029+IPS-DataManager@users.noreply.github.com>
2022-03-11 16:53:37 -0600

    Create IPS_Dashboard_6.py

---
+++ IPS_Dashboard_6.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 09:09:48 2022
+
+@author: IPSMX-L7NRKD03
+"""
+#Core Packages
+import streamlit as st
+from PIL import Image
+import streamlit_authenticator as stauth
+
+#EDA
+import pandas as pd 
+import numpy as np
+
+#Graphers
+from plotly.subplots import make_subplots
+import plotly.express as px
+import plotly.graph_objs as go
+
+pd.options.mode.chained_assignment = None  # default='warn'
+
+######################################## Page Config ########################################
+APP_TITLE = "IPS Dashboard"
+img=Image.open('Imagenes\IPS.png')
+st.set_page_config(
+    page_title = APP_TITLE,
+    page_icon = img,
+    layout = "wide")
+
+img_sidebar= st.sidebar.columns(3)
+img_sidebar[1].image(img,width=100)
+
+######################################## Authentication ########################################
+users = pd.read_csv('Usuarios//Usuarios.csv', encoding='utf-8')
+users['pass'] = users['pass'].astype(str)
+users['pass'] = '000' + users['pass']
+users
+hashed_passwords = stauth.hasher(users['pass']).generate()
+authenticator = stauth.authenticate(users['nombre_c'], users['usuario'], hashed_passwords, 'IPS_Dashboard','key_1', cookie_expiry_days=30)
+
+name, authentication_status = authenticator.login('Login','main')
+
+if st.session_state['authentication_status']:
+    st.write('Bienvenido *%s*' % (st.session_state['name']))
+    # st.title('Some content')
+elif st.session_state['authentication_status'] == False:
+    st.error('Username/password is incorrect')
+elif st.session_state['authentication_status'] == None:
+    st.warning('Please enter your username and password')
