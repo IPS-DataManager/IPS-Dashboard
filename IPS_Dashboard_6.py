b0f966380f2eb802579625810081e3c455d66236
IPS Data Manager <101430029+IPS-DataManager@users.noreply.github.com>
2022-03-29 08:49:56 -0600

    Update IPS_Dashboard_6.py

--- IPS_Dashboard_6.py
+++ IPS_Dashboard_6.py
@@ -59,7 +59,9 @@ name, authentication_status = authenticator.login('Login','main')
 if st.session_state['authentication_status']:
     st.write('Bienvenido *%s*' % (st.session_state['name']))
     st.title('Tablero de Campos Maduros - Proyecto Sitio Grande')
-    prod
+    with st.container():
+        with st.expander('DATA)'):
+            prod
     
 ################################################################################################################################################################
 elif st.session_state['authentication_status'] == False:
