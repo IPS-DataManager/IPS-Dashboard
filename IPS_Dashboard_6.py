ebf9df9201dd422ed1c66c199642d2020fd12d9f
IPS Data Manager <101430029+IPS-DataManager@users.noreply.github.com>
2022-04-08 09:44:38 -0500

    Update IPS_Dashboard_6.py

--- IPS_Dashboard_6.py
+++ IPS_Dashboard_6.py
@@ -123,6 +123,7 @@ if st.session_state['authentication_status']:
             shots
             
         with st.expander('GRÁFICOS DE PRODUCTIVIDAD'):
+            prod_plots = st.columns(2)
             w_prod_plot = make_subplots(specs=[[{"secondary_y": True}]])
             w_prod_plot.add_trace(go.Scatter(x=pozo['Fecha'], y=pozo['Aceite_bpd'], mode='lines', marker_line_width=.5, marker=dict(size=5,color='green'),name='Aceite'),secondary_y=False)
             w_prod_plot.add_trace(go.Scatter(x=pozo['Fecha'], y=pozo['Agua_bpd'], mode='lines', marker_line_width=.5, marker=dict(size=5,color='blue'), name='Agua'), secondary_y=False)
@@ -131,9 +132,9 @@ if st.session_state['authentication_status']:
             w_prod_plot.update_yaxes(title_text="<b> Aceite [SBPD] / Agua [SBPD] </b> ", secondary_y=False, nticks=10)
             w_prod_plot.update_yaxes(title_text="<b> Gas [MMSPCD]</b>", secondary_y=True, nticks=10)
             w_prod_plot.update_xaxes(title_text="<b>Año</b>", nticks=25)
-            w_prod_plot
+            prod_plots[0].w_prod_plot
             if st.button(f'Exportar Gráfico de Producción Histórica {filt_pozos}') == True:
-                well_prod_2.write_html(f'{filt_pozos} Histórico de Producción.html')
+                prod_plots[0].well_prod_2.write_html(f'{filt_pozos} Histórico de Producción.html')
             
 ################################################################################################################################################################
 elif st.session_state['authentication_status'] == False:
