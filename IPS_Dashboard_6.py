333d8a00ebeff9434dddd19a25976094bfafe3c5
IPS Data Manager <101430029+IPS-DataManager@users.noreply.github.com>
2022-04-08 13:26:29 -0500

    Update IPS_Dashboard_6.py

--- IPS_Dashboard_6.py
+++ IPS_Dashboard_6.py
@@ -131,7 +131,7 @@ if st.session_state['authentication_status']:
             w_prod_plot.add_trace(go.Scatter(x=pozo['Fecha'], y=pozo['Aceite_bpd'], mode='lines', marker_line_width=.5, marker=dict(size=5,color='green'),name='Aceite'),secondary_y=False)
             w_prod_plot.add_trace(go.Scatter(x=pozo['Fecha'], y=pozo['Agua_bpd'], mode='lines', marker_line_width=.5, marker=dict(size=5,color='blue'), name='Agua'), secondary_y=False)
             w_prod_plot.add_trace(go.Scatter(x=pozo['Fecha'], y=pozo['Gas_mmcfpd'], mode='lines', marker_line_width=.5, marker=dict(size=5,color='red'),name='Gas'),secondary_y=True)
-            w_prod_plot.update_layout(title_text = f'Histórico de producción {filt_pozos}', hovermode="x unified", font=dict(family="sans-serif", size=15, color="black"), margin={"r":0,"t":100,"l":0,"b":0}, height=500, width=1000)
+            w_prod_plot.update_layout(title_text = f'Histórico de producción {filt_pozos}', hovermode="x unified", font=dict(family="sans-serif", size=15, color="black"), legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="left", x=1), margin={"r":0,"t":100,"l":0,"b":0}, height=500, width=1000)
             w_prod_plot.update_yaxes(title_text="<b> Aceite [SBPD] / Agua [SBPD] </b> ", secondary_y=False, nticks=10)
             w_prod_plot.update_yaxes(title_text="<b> Gas [MMSPCD]</b>", secondary_y=True, nticks=10)
             w_prod_plot.update_xaxes(title_text="<b>Año</b>", nticks=25)
@@ -154,7 +154,7 @@ if st.session_state['authentication_status']:
                     overlaying="y",
                     side="right",
                     position=1))
-            raa_rga_plot.update_layout(title_text=f'RGA/RAA {filt_pozos}', height=500, width=1030, font=dict(family="sans-serif", size=15, color="black"))
+            raa_rga_plot.update_layout(title_text=f'RGA/RAA {filt_pozos}', height=500, width=1030, font=dict(family="sans-serif", size=15, color="black"), legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="left", x=1))
             raa_rga_plot.update_yaxes(rangemode="tozero")
             prod_plots[0].plotly_chart(raa_rga_plot)
 
