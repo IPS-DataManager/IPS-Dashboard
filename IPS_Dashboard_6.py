913e65ccb22ddbf1386343cba9866da30f75b0b0
IPS Data Manager <101430029+IPS-DataManager@users.noreply.github.com>
2022-04-09 12:30:44 -0500

    Update IPS_Dashboard_6.py

--- IPS_Dashboard_6.py
+++ IPS_Dashboard_6.py
@@ -45,7 +45,7 @@ days = 30.5
 # Miles
 #M = 1000
 # Millones
-#MM = 1000000
+MM = 1000000
 
 ######################################## Authentication ########################################
 users = pd.read_csv('Usuarios//Usuarios.csv', encoding='utf-8')
@@ -156,26 +156,31 @@ if st.session_state['authentication_status']:
             raa_rga_plot.update_layout(title_text=f'RGA/RAA {filt_pozos}', height=350, width=1000, font=dict(family="sans-serif", size=10, color="black"), legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="right", x=1))
             raa_rga_plot.update_yaxes(rangemode="tozero")
             st.plotly_chart(raa_rga_plot)
-
+            
+            cum_prod = st.columns(2)
             cum_oil_plot = px.ecdf(pozo, x="Fecha", y="Aceite_bpm", ecdfnorm=None)
             cum_oil_plot.update_layout(font=dict(family="sans-serif", size=10, color="black"), hovermode="x unified", title=f'Producción de Aceite Acumulado {filt_pozos}', legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="right", x=1), margin={"r":0,"t":50,"l":0,"b":0}, height=200, width=500)
             cum_oil_plot.update_traces(marker=dict(color='green'))
             cum_oil_plot.update_yaxes(title_text="<b>Aceite [bbls]</b>", nticks=10)
             cum_oil_plot.update_xaxes(title_text="<b>Años</b>", nticks=10)
-            cum_oil_plot
+            cum_prod[0].cum_oil_plot
             cum_water_plot = px.ecdf(pozo, x="Fecha", y="Agua_bpm", ecdfnorm=None)
             cum_water_plot.update_layout(font=dict(family="sans-serif", size=10, color="black"), hovermode="x unified", title=f'Producción de Agua Acumulada {filt_pozos}', legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="right", x=1), margin={"r":0,"t":50,"l":0,"b":0}, height=200, width=500)
             cum_water_plot.update_traces(marker=dict(color='blue'))
             cum_water_plot.update_yaxes(title_text="<b>Agua [bbls]</b>", nticks=10)
             cum_water_plot.update_xaxes(title_text="<b>Años</b>", nticks=10)
-            cum_water_plot                      
+            cum_prod[0].cum_water_plot                      
             cum_gas_plot = px.ecdf(pozo, x="Fecha", y="Gas_mmcfpm", ecdfnorm=None)
             cum_gas_plot.update_layout(font=dict(family="sans-serif", size=10, color="black"), hovermode="x unified", title=f'Producción de Gas Acumulado {filt_pozos}', legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="right", x=1), margin={"r":0,"t":50,"l":0,"b":0}, height=200, width=500)
             cum_gas_plot.update_traces(marker=dict(color='red'))
             cum_gas_plot.update_yaxes(title_text="<b>Gas [MMPCPD]</b>", nticks=10)
             cum_gas_plot.update_xaxes(title_text="<b>Años</b>", nticks=10)
-            cum_gas_plot
+            cum_prod[0].cum_gas_plot
             
+            cum_prod[1].caption("Accumulated Oil Production: " + str(round(pozo['Aceite_bpm'].sum()/MM,2)) + " Million Barrels")
+            cum_prod[1].caption("Accumulated Water Production: " + str(round(pozo['Agua_bpm'].sum()/MM,2)) + " Million Barrels")
+            cum_prod[1].caption("Accumulated Gas Production: " + str(round(pozo['gas_mmcfpm'].sum(),2)) + " Million Cubic Feet")
+                        
             if st.button(f'Exportar Gráfico de Producción Histórica {filt_pozos}') == True:
                 st.well_prod_2.write_html(f'{filt_pozos} Histórico de Producción.html')
             if st.button(f'Exportar Gráfico de RAA-RGA Histórica {filt_pozos}') == True:
