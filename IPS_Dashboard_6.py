c90719870f8ba361c9c90f46656881960c1ea492
IPS Data Manager <101430029+IPS-DataManager@users.noreply.github.com>
2022-04-09 14:52:25 -0500

    Update IPS_Dashboard_6.py

--- IPS_Dashboard_6.py
+++ IPS_Dashboard_6.py
@@ -145,7 +145,7 @@ if st.session_state['authentication_status']:
             well_prod_press.add_trace(go.Scatter(x=pozo['Fecha'], y=pozo['Agua_bpd'], name="Agua", mode='lines', marker_line_width=.3, marker=dict(size=5,color='blue'), yaxis="y2"))
             well_prod_press.add_trace(go.Scatter(x=pozo['Fecha'], y=pozo['Gas_mmcfpd'], name="Gas", mode='lines', marker_line_width=.3, marker=dict(size=5,color='red'), yaxis="y3"))
             well_prod_press.add_trace(go.Scatter(x=press_pozo['Fecha'], y=press_pozo['De fondo cerrado gradiente de yacimiento (kg/cm2)'], line={'dash': 'dash', 'color': 'DarkSlateGrey'}, name="Presión", mode='lines', marker_line_width=.3, yaxis="y4"))
-            well_prod_press.update_layout(title_text=f'HISTÓRICO DE PRODUCCIÓN {filt_pozos}', height=400, width=1150, font=dict(family="sans-serif", size=10, color="black"), legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="right", x=1))
+            well_prod_press.update_layout(title_text=f'HISTÓRICO DE PRODUCCIÓN {filt_pozos}', height=400, width=1350, font=dict(family="sans-serif", size=10, color="black"), legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="right", x=1))
             well_prod_press.update_layout(hovermode="x unified", margin={"r":0,"t":100,"l":0,"b":0}, xaxis=dict(title_text="<b>Año</b>", nticks=25, domain=[0.2, 0.8]),
                 yaxis=dict(nticks=20, exponentformat='none', title="Aceite [SBPD]", titlefont=dict(color="black", size=12), tickfont=dict(color="black", size=9)),
                 yaxis2=dict(nticks=30, exponentformat='none', title="Agua [SBPD]", titlefont=dict(color="black", size=12), tickfont=dict(color="black", size=9),
@@ -162,6 +162,7 @@ if st.session_state['authentication_status']:
                         overlaying="y",
                         side="right",
                         position=0.85))
+            well_prod_press.update_yaxes(rangemode="tozero")
             st.plotly_chart(well_prod_press)
             
             raa_rga_plot = go.Figure()
@@ -170,14 +171,14 @@ if st.session_state['authentication_status']:
             raa_rga_plot.add_trace(go.Scatter(x=pozo['Fecha'], y=pozo['Wor'], name="RAA", mode='lines', marker_line_width=.3, marker=dict(size=5,color='skyblue'), yaxis="y4"))
             raa_rga_plot.update_layout(title_text=f'RGA/RAA {filt_pozos}', height=350, width=1050, font=dict(family="sans-serif", size=10, color="black"), legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="right", x=1))
             raa_rga_plot.update_layout(hovermode="x unified", margin={"r":0,"t":100,"l":100,"b":0}, xaxis=dict(title_text="<b>Año</b>", nticks=25, domain=[0, 0.95]),
-                yaxis=dict(nticks=20, exponentformat='none', title="<b>RGA [sm3/sm3]</b>", titlefont=dict(
-                        color="black", size=10), tickfont=dict(color="black", size=9)),
-                yaxis3=dict(nticks=20, exponentformat='none', title="<b>Corte de Agua [%]</b>", titlefont=dict(color="black", size=10), tickfont=dict(color="black", size=9),
+                yaxis=dict(nticks=20, exponentformat='none', title="RGA [sm3/sm3]", titlefont=dict(
+                        color="black", size=12), tickfont=dict(color="black", size=9)),
+                yaxis3=dict(nticks=20, exponentformat='none', title="Corte de Agua [%]", titlefont=dict(color="black", size=12), tickfont=dict(color="black", size=9),
                     anchor="x",
                     overlaying="y",
                     side="right",
                     position=0.95),
-                yaxis4=dict(nticks=20,exponentformat='none', title="<b>RAA [sm3/sm3]</b>", titlefont=dict(color="black", size=10), tickfont=dict(color="black", size=9),
+                yaxis4=dict(nticks=20,exponentformat='none', title="RAA [sm3/sm3]", titlefont=dict(color="black", size=12), tickfont=dict(color="black", size=9),
                     anchor="free",
                     overlaying="y",
                     side="right",
